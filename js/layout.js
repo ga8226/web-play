0857547c47956cd7c3989e2b8a5dec82d29e87f6
ga826 <ga8226@naver.com>
2023-07-19 16:29:10 +0900

    페이드로 사라지는거 만들기 css랑 js 수정

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,15 @@
 $(function(){
+    let count =0;
     setInterval(function(){
-        $(".ani").animate({"marginLeft" : -800}, 400,function(){
-            $(this).find("div").eq(0).appendTo($(this))
-            //첫번째 div를 맨끝으로 옮겨라
-            $(this).css("margin-left",0)
-            //원래자리로
-        })
+        // $(".ani").animate({"marginLeft" : -800}, 400,function(){
+        //     $(this).find("div").eq(0).appendTo($(this))
+        //     //첫번째 div를 맨끝으로 옮겨라
+        //     $(this).css("margin-left",0)
+        //     //원래자리로
+        // })
+        count++;
+        count %=3;
+        $(".ani>div").eq(count).addClass("on").siblings().removeClass("on")
     },3000)
    
 })
